9014cdbea41d08147e9fa9c9bb573d42b7ef5aaa
bigmamapr0 <lipchev02@abv.bg>
2020-10-13 20:29:48 +0300

    first init

---
+++ src/game.ts
@@ -0,0 +1,28 @@
+import 'phaser';
+import { Boot } from './scenes/Boot';
+import { Preload } from './scenes/Preload';
+
+class GameApp extends Phaser.Game {
+    public static gameConfig: Phaser.Types.Core.GameConfig = null;
+
+    constructor(config: Phaser.Types.Core.GameConfig) {
+        GameApp.gameConfig = config;
+
+        if (GameApp.gameConfig == null) {
+            GameApp.gameConfig = {
+                type: Phaser.AUTO,
+                parent: "content",
+                backgroundColor: '#385e78',
+                width: 1024,
+                height: 512,
+                scene: [Boot, Preload]
+            };
+        }
+
+        super(GameApp.gameConfig);
+    }
+}
+
+export { GameApp }
+
+new GameApp(null);
