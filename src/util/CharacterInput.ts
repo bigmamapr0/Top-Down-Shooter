1535d33cccf60a8008730fc6b971f1a50784230d
bigmamapr0 <lipchev02@abv.bg>
2020-10-17 23:04:53 +0300

    added keyboard input

---
+++ src/util/CharacterInput.ts
@@ -0,0 +1,20 @@
+class CharacterInput {
+
+    public w?: Phaser.Input.Keyboard.Key;
+    public s?: Phaser.Input.Keyboard.Key;
+    public a?: Phaser.Input.Keyboard.Key;
+    public d?: Phaser.Input.Keyboard.Key;
+
+    public space?: Phaser.Input.Keyboard.Key;
+
+    constructor(scene: Phaser.Scene) {
+        this.w = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W);
+        this.s = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S);
+        this.a = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A);
+        this.d = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D);
+
+        this.space = scene.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+    }
+}
+
+export { CharacterInput };
