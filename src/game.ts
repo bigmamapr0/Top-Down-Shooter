7602f3fa97021f67300ec2eb287b1f6f5a3ae7e5
vidons <vidons@gmail.com>
2020-10-18 11:54:24 +0300

    resolve merge conflict

--- src/game.ts
+++ src/game.ts
@@ -1,14 +1,9 @@
 import 'phaser';
-<<<<<<< HEAD
 import { Boot } from './scenes/LoadingScenes/Boot';
 import { Preload } from './scenes/LoadingScenes/Preload';
 import { Gameplay } from './scenes/Gameplay/Gameplay';
-=======
-import { Boot } from './scenes/Boot';
-import { Preload } from './scenes/Preload';
 import { MainMenu } from './scenes/MainMenu';
 import { Level } from './scenes/Level';
->>>>>>> origin/ultra
 
 class GameApp extends Phaser.Game {
     public static gameConfig: Phaser.Types.Core.GameConfig = null;
@@ -21,26 +16,15 @@ class GameApp extends Phaser.Game {
                 type: Phaser.AUTO,
                 parent: "content",
                 backgroundColor: '#385e78',
-<<<<<<< HEAD
                 width: window.innerWidth,
                 height: window.innerHeight,
                 physics: {
                     default: "arcade",
-=======
-                width: 1024,
-                height: 512,
-                physics: {
-                    default: 'arcade',
->>>>>>> origin/ultra
                     arcade: {
                         debug: true
                     }
                 },
-<<<<<<< HEAD
-                scene: [Boot, Preload, Gameplay]
-=======
                 scene: [Boot, Preload, MainMenu, Level]
->>>>>>> origin/ultra
             };
         }
 
