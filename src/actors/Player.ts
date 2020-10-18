f8478351e5a192695efc78c4ccf5a661e26c734b
bigmamapr0 <lipchev02@abv.bg>
2020-10-18 12:47:43 +0300

    player movement improvements

--- src/actors/Player.ts
+++ src/actors/Player.ts
@@ -13,6 +13,8 @@ class Player extends Phaser.Physics.Arcade.Sprite {
     constructor(scene: Phaser.Scene, x, y, texture, frame) {
         super(scene, x, y, texture, frame);
 
+        this.setScale(0.5);
+
         this.scene.anims.create({
             key: "idleRifle",
             frameRate: 10,
@@ -26,7 +28,7 @@ class Player extends Phaser.Physics.Arcade.Sprite {
 
         this.scene.anims.create({
             key: "walkRifle",
-            frameRate: 10,
+            frameRate: 25,
             frames: this.scene.anims.generateFrameNames("playerWalkRifle", {
                 prefix: "survivor-move_rifle_",
                 start: 0,
@@ -49,16 +51,44 @@ class Player extends Phaser.Physics.Arcade.Sprite {
 
         if (this.keys.w.isDown) {
             this.setVelocityY(-this.movementSpeed);
+            this.rotation = -3.14/2;
         } else if (this.keys.s.isDown) {
             this.setVelocityY(this.movementSpeed);
+            this.rotation = 3.14/2;
         }
 
         if (this.keys.a.isDown) {
             this.setVelocityX(-this.movementSpeed);
+            this.rotation = +-3.14;
         } else if (this.keys.d.isDown) {
             this.setVelocityX(this.movementSpeed);
+            this.rotation = 0;
         }
 
+        if (this.keys.w.isDown || this.keys.s.isDown || this.keys.a.isDown || this.keys.d.isDown) {
+            this.play("walkRifle", true);
+        } else {
+            this.play("idleRifle", true);
+        }
+
+        // Side Ways Animation
+        if (this.keys.w.isDown && this.keys.d.isDown) {
+            this.rotation = (-3.14/2) / 2;
+        }
+
+        if (this.keys.w.isDown && this.keys.a.isDown) {
+            this.rotation = 180
+        }
+        
+        if (this.keys.s.isDown && this.keys.d.isDown) {
+            this.rotation = (3.14/2) / 2;
+        }
+        
+        if (this.keys.s.isDown && this.keys.a.isDown) {
+            this.rotation = 90
+        }
+
+        // Sprinting
         if (this.keys.shift.isDown) {
             this.movementSpeed = this.sprintSpeedAdded;
         } else if (this.keys.shift.isUp) {
