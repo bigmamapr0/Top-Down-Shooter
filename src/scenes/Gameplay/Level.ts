7e5bd867f5d4a2cc16251c1a3dac511d963128be
vidons <vidons@gmail.com>
2020-10-21 20:04:09 +0300

    added attack

--- src/scenes/Gameplay/Level.ts
+++ src/scenes/Gameplay/Level.ts
@@ -2,6 +2,7 @@ import { CharacterInput } from "../../util/CharacterInput";
 import { Soldier } from "../../actors/enemies/Soldier";
 import { Helicopter } from "../../actors/enemies/Helicopter";
 import { Acs } from "../../actors/enemies/Acs";
+import { Bomber } from "../../actors/enemies/Bomber";
 import { Player } from "../../actors/Player";
 
 class Level extends Phaser.Scene {
@@ -18,12 +19,17 @@ class Level extends Phaser.Scene {
 
     private acs: Acs;
 
+    private mouse: Phaser.Input.Pointer;
+    public input: Phaser.Input.InputPlugin;
+
+    private bomber: Bomber;
     constructor() {
         super("level");
     }
 
     create() {
         this.keys = new CharacterInput(this);
+        this.mouse = this.input.mousePointer;
 
         this.player = new Player(this, 200, 200, "playerIdleRifle", "survivor-idle_rifle_4");
         this.add.existing(this.player);
@@ -47,6 +53,9 @@ class Level extends Phaser.Scene {
         this.helicopter.anims.play("helicopterRotor");
         this.add.existing(this.helicopter);
 
+        // this.bomber = new Bomber(this, 800, 200);
+        // this.add.existing(this.bomber);
+
         this.time.addEvent({
             delay: 1000,
             callback: this.soldier_die,
@@ -65,6 +74,14 @@ class Level extends Phaser.Scene {
     }
 
     update() {
+        if (this.mouse.isDown) {
+            let bullet = this.physics.add.sprite(this.player.x, this.player.y, "enemies", "soldierBullet");
+
+            let angleBullet = Phaser.Math.Angle.Between(this.player.x, this.player.y, this.input.x, this.input.y);
+            bullet.setRotation(angleBullet+1.5708);
+            this.physics.moveTo(bullet, this.input.x, this.input.y, 100);
+        }
+
         this.player.update();
         this.soldier.update();
         this.soldier2.update();
